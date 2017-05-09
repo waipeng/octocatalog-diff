4eff6aa4122a13104f99ac5a8ba6ce42c5811989
Jake Yip <jake.yip@unimelb.edu.au>
2017-05-09 17:22:25 +1000

    Add filter to ignore diff between Type and [Type]
    Puppet 3 creates some resources as e.g. Service[httpd] but Puppet 4
    creates them in an array like ["Service[httpd]"]. Adds a filter that
    ignores the differences between these two.

--- lib/octocatalog-diff/catalog-diff/filter.rb
+++ lib/octocatalog-diff/catalog-diff/filter.rb
@@ -1,5 +1,6 @@
 require_relative '../api/v1/diff'
 require_relative 'filter/absent_file'
+require_relative 'filter/array_single_value'
 require_relative 'filter/compilation_dir'
 require_relative 'filter/json'
 require_relative 'filter/yaml'
@@ -13,7 +14,7 @@ module OctocatalogDiff
       attr_accessor :logger
 
       # List the available filters here (by class name) for use in the validator method.
-      AVAILABLE_FILTERS = %w(AbsentFile CompilationDir JSON YAML).freeze
+      AVAILABLE_FILTERS = %w(AbsentFile ArraySingleValue CompilationDir JSON YAML).freeze
 
       # Public: Determine whether a particular filter exists. This can be used to validate
       # a user-submitted filter.
